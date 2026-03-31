2a3b42bfaf9f6c88cd2a900b7604b3674a95b92c
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2026-03-31 15:14:00 +0200

    chore: gitignore playwright logs, _old backup, and research docs

--- storefront/_old/src/lib/sdk.ts
+++
@@ -1,6 +0,0 @@
-import Medusa from "@medusajs/js-sdk"
-
-export const sdk = new Medusa({
-  baseUrl: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || "http://localhost:9000",
-  publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
-})
